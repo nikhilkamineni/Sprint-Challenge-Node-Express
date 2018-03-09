1f736d2419b19fe5bbe04164b04b4e1a4681338a
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-09 13:09:20 -0500

    /compare endpoint is now outputting object with current, yesterday and change

--- src/app.js
+++ src/app.js
@@ -6,34 +6,37 @@ const server = express();
 
 const PORT = 3030;
 
-const URL_CURRENT = "https://api.coindesk.com/v1/bpi/currentprice.json"
-const URL_YEST = 'https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday'
+const URL_CURRENT = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+const URL_YEST =
+  'https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday';
 
 server.get('/current', (req, res) => {
   fetch(URL_CURRENT)
     .then(res => res.json())
     .then(current => {
-      res.status(200)
-      res.send(current)
+      res.status(200);
+      res.send(current);
     })
     .catch(err => {
-      res.status(422)
-      res.send(`Error getting current price: ${err}`)
-    })
-})
+      res.status(422);
+      res.send(`Error getting current price: ${err}`);
+    });
+});
 
 server.get('/compare', (req, res) => {
   getCurrentValue()
     .then(value => {
-      res.status(200)
-      res.send({ value: value })
+      getYestValue(value)
+        .then(values => {
+          res.status(200);
+          res.send(values);
+      })
     })
     .catch(err => {
-      res.status(422)
-      res.send({ error: 'Error in /compare request' })
-    })
-    
-})
+      res.status(422);
+      res.send({error: 'Error in /compare request'});
+    });
+});
 
 function getCurrentValue() {
   return new Promise((resolve, reject) => {
@@ -45,15 +48,36 @@ function getCurrentValue() {
       })
       .catch(err => {
         reject(err);
-      })
-  })
+      });
+  });
 }
 
-server.listen(PORT, (err) => {
+function getYestValue(current) {
+  return new Promise((resolve, reject) => {
+    fetch(URL_YEST)
+      .then(res => res.json())
+      .then(res => res.bpi)
+      .then(bpi => {
+        const yestValue = Object.values(bpi)[0];
+        const change = current - yestValue;
+        const response = {
+          current: current,
+          yesterday: yestValue,
+          change: change
+        };
+
+        resolve(response);
+      })
+      .catch(err => {
+        reject(err);
+      })
+  });
+}
+
+server.listen(PORT, err => {
   if (err) {
     console.log(`Error starting server: ${err}`);
   } else {
     console.log(`Server listening on port ${PORT}`);
   }
-})
-
+});
