926281798f7c3352945d19efedc360886d7f7b78
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-09 12:40:47 -0500

    getCurrentValue function working

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const server = express();
 const PORT = 3030;
 
 const URL_CURRENT = "https://api.coindesk.com/v1/bpi/currentprice.json"
+const URL_YEST = 'https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday'
 
 server.get('/current', (req, res) => {
   fetch(URL_CURRENT)
@@ -20,6 +21,33 @@ server.get('/current', (req, res) => {
       res.send(`Error getting current price: ${err}`)
     })
 })
+
+server.get('/compare', (req, res) => {
+  getCurrentValue()
+    .then(value => {
+      res.status(200)
+      res.send({ value: value })
+    })
+    .catch(err => {
+      res.status(422)
+      res.send({ error: 'Error in /compare request' })
+    })
+    
+})
+
+function getCurrentValue() {
+  return new Promise((resolve, reject) => {
+    fetch(URL_CURRENT)
+      .then(res => res.json())
+      .then(res => res.bpi.USD.rate_float)
+      .then(rate => {
+        resolve(rate);
+      })
+      .catch(err => {
+        reject(err);
+      })
+  })
+}
 
 server.listen(PORT, (err) => {
   if (err) {
